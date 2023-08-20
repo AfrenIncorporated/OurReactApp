a8d9f914620596d3292d31dec51d9117e147a04e
Raheem Abdulhaqq <raheemabdulhaqq2020@gmail.com>
2023-08-20 20:50:59 +0100

    added news page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './Pages/Login'
 import Signup from './Pages/Signup'
 import About from './Pages/About'
 import ClientReg from './Pages/ClientReg'
+import News from './Pages/News'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/client-registration' element={<ClientReg/>}/>
+        <Route path='/news' element={<News/>}/>
       </Routes>
     </Router>
   )
